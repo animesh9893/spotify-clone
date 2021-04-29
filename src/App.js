4be13b596ae4d599cbae95cac8f529490cd72d07
Animesh Shrivatri <animesh.shrivatri2002@gmail.com>
2021-04-29 21:40:04 +0530

    Playlist extraction complete

--- src/App.js
+++ src/App.js
@@ -9,38 +9,43 @@ import {useDataLayerValue} from './DataLayer'
 const spotify = new SpotifyWebApi();
 
 function App() {
-  const [token,setToken] = useState(null);
 
-  const [{user},dispatch] = useDataLayerValue();
+  const [{user,token},dispatch] = useDataLayerValue();
 
   useEffect(()=>{
     const hash= getTokenFromUrl();
     window.location.hash="";
     const _token = hash.access_token;
     if (_token){
-      setToken(_token)
+      dispatch({
+          type: 'SET_TOKEN',
+          token: _token,
+      })
 
 
       spotify.setAccessToken(_token)
       
       spotify.getMe().then(user =>{
-        console.log(user)
         dispatch({
           type: 'SET_USER',
           user: user
       })
 
       })
-
+      spotify.getUserPlaylists().then((playlists)=>{
+        dispatch({
+          type: "SET_PLAYLISTS",
+          playlists:playlists,
+        })
+      })
     }
-    console.log(token)
   },[token]);  
-  console.log(user)
+
   return (
     <div className="App">
       {
         token ? (
-          <Player />
+          <Player spotify={spotify}/>
         ):(
           <Login />
         )
--- src/reducer.js
+++ src/reducer.js
@@ -1,8 +1,11 @@
 export const initialState={
 	user:null,
-	playlist:[],
+	playlists:[],
 	playing:false,
 	item:null,
+	// token: 'BQC4aRw0vtdZFSCUmEkWjgDeFX35L_YSgt6D_-D3IyMU-1A7bTaw88OiUt54cvLlwjjv4h7ISDPdLLmeWg3IhAqk6WW1vxcOjrZhOg0nAUfDViyYDVgGP5R34USqR5x4hMZObZcLv1cxNJblXHZHthk99KZLE7fvpmqvoAZ3pHZJugNYejXO',
+	token: null,
+
 }
 
 const reducer = (state,action) =>{
@@ -12,6 +15,16 @@ const reducer = (state,action) =>{
 			return {
 				...state,
 				user: action.user,
+			}
+		case 'SET_TOKEN':
+			return {
+				...state,
+				token: action.token,
+			}
+		case 'SET_PLAYLISTS':
+			return {
+				...state,
+				playlists: action.playlists,
 			}
 		default:
 			return state;
