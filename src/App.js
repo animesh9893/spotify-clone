887b5479ff30929d6a5504e3afed1b0f286d0ef9
Animesh Shrivatri <animesh.shrivatri2002@gmail.com>
2021-04-29 17:41:57 +0530

    login set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import Login from './Login'
 import './App.css';
+import React, {useEffect, useState} from 'react';
+import {getTokenFromUrl} from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js'
+import Player from './Player'
+import {useDataLayerValue} from './DataLayer'
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+  const [token,setToken] = useState(null);
+
+  const [{user},dispatch] = useDataLayerValue();
+
+  useEffect(()=>{
+    const hash= getTokenFromUrl();
+    window.location.hash="";
+    const _token = hash.access_token;
+    if (_token){
+      setToken(_token)
+
+
+      spotify.setAccessToken(_token)
+      
+      spotify.getMe().then(user =>{
+        console.log(user)
+        dispatch({
+          type: 'SET_USER',
+          user: user
+      })
+
+      })
+
+    }
+    console.log(token)
+  },[token]);  
+  console.log(user)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        token ? (
+          <Player />
+        ):(
+          <Login />
+        )
+      }
     </div>
   );
 }
